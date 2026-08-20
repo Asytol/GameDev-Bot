037cc9cba7afebf855d3ea2d6eac70bacc4692e4
Asitol <ohlsadam09@gmail.com>
2026-08-20 22:20:02 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Hosting;
+
+using NetCord.Hosting.Gateway;
+
+namespace GameDevBot
+{
+    internal class MainProgram
+    {
+        static async Task Main(string[] args){
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+            builder.Services.AddDiscordGateway();
+
+            IHost host = builder.Build();
+
+            await host.RunAsync();
+
+            Console.WriteLine("test");
+        }
+    }
+}
